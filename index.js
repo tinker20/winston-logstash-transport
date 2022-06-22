7c8d41b0de78f0554f33411b6476177ef7db37f6
smandal-godaddy <105301744+smandal-godaddy@users.noreply.github.com>
2022-06-22 16:12:18 +0530

    Update index.js
    Make file available for 2 days

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const logger = function (scope) {
         datePattern: 'YYYY-MM-DD-HH',
         zippedArchive: false,
         maxSize: '20m',
-        maxFiles: '7d'
+        maxFiles: '2d'
       }));
       log = winston.createLogger({
         level: CONSTANTS.LOG_LEVEL,
